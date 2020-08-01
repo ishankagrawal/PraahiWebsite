40ce3e71b47cc2fc3f00c838ef9c6393b5d463ef
Ishank Agrawal <iagrawal784@gmail.com>
2020-08-01 20:25:01 +0530

    added homepage

--- Jquery/jq.js
+++ Jquery/jq.js
@@ -2,21 +2,25 @@ $(document).ready(function()
 {
 	$(".l").hover(function()
 		{
-		
+		$(".left").animate({backgroundSize: '160%'},"fast");
 		$(this).css("opacity","100%");
+		
 		},
 		function(){
-			console.log("kyaaa??");
+			
 			$(this).css("opacity","0%");
+			$(".left").animate({backgroundSize : '150%'},"fast");
 		});
 
 	$(".r").hover(function()
 		{
 		$(this).css("opacity","100%");
+		$(".right").animate({backgroundSize: '150%'},"fast");
 		},
 		function(){
 			
 			$(this).css("opacity","0%");
+			$(".right").animate({backgroundSize: '130%'},"fast");
 		});
 })
 
